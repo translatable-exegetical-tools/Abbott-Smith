1ca5609bdd78a3ea93232db4163d94e683217f8b
Daniel Owens <dcowens76@gmail.com>
2014-01-08 12:26:09 +0800

    fixes for validation

--- scripts/generateabbottsmith.sh
+++ scripts/generateabbottsmith.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 # This validates the TEI file against the schema. 
-xmllint --valid --noout abbott-smith.tei.xml --schema ~/Documents/Work/BibleTech/OSIS/osisCore.2.1.1.xsd
+xmllint --valid --noout ../abbott-smith.tei.xml --schema http://www.crosswire.org/OSIS/teiP5osis.2.5.0.xsd
  
