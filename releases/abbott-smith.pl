523ff5a59eec9514969d48a621cd9f5074e25b4c
dowens76 <dcowens76@gmail.com>
2015-03-08 15:00:24 +0700

    Clean up Perl code
    Clean up Perl script to prepare document for release.

--- releases/abbott-smith.pl
+++ releases/abbott-smith.pl
@@ -2,18 +2,18 @@
 open (INF, "../abbott-smith.tei.xml");
 my @data = <INF>;
 close (INF);
-open (OUTF, ">abbott-smith.v0.16.xml");
+open (OUTF, ">abbott-smith.tei-current.xml");
 
 my $line;
 foreach $line (@data) {	
 
+	# Why is this here?  
 	$line =~ s/<pb/\n<pb/g;
+	
+	# Remove notes about checked pages (not needed for a release).
 	$line =~ s/ <!-- typed="([^"]*)" checked="([^"]*)" -->//g;
 	$line =~ s/ <!-- checked="([^"]*)" -->//g;
-#	$line =~ s/^\s*([^<|\s])(.+)\n//g;
-#	$line =~ s/\s*\n\s*\n\s*/\n/g;
-#	$line =~ s/^(;|\+|:|t)\n//g;
-
+	
 	print OUTF "$line";
 }
 close (OUTF);
