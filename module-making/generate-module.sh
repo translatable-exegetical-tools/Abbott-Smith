3042979abea049d88eb6a419bf14ac422176daa4
dowens76 <dcowens76@gmail.com>
2017-02-08 08:15:48 +0700

    Updated module script

--- module-making/generate-module.sh
+++ module-making/generate-module.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 
 ### Bundles source xml file and conf file.
-zip abbott-smith.tei-current.zip abbott-smith.tei-simple.xml mods.d/abbottsmith.conf
+zip abbottsmithstrongs-current.zip abbott-smith.tei-strongs.xml mods.d/abbottsmithstrongs.conf
 
 ### Compiles the lexicon.
-tei2mod modules/lexdict/zld/abbottsmithstrongs/ abbott-smith.tei-simple.xml -z
+tei2mod modules/lexdict/zld/abbottsmithstrongs/ abbott-smith.tei-strongs.xml -z
 #tei2mod modules/lexdict/zld/abbottsmith/ abbott-smith.tei-simple.xml
 
 ### Bundles the module.
-zip sword.zip ./mods.d/abbottsmith.conf ./modules/lexdict/zld/abbottsmith/*
+zip sword.zip ./mods.d/abbottsmithstrongs.conf ./modules/lexdict/zld/abbottsmithstrongs/*
 
 #rm abbott-smith.tei-simple.xml 
 #rm abbott-smith.tei-current.xml
